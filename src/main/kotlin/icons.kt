72e0a86c3a0656a20b9b83a9d524c51f41835552
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-10-08 17:02:44 +0900

    add icons

---
+++ src/main/kotlin/icons.kt
@@ -0,0 +1,8 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    @JvmField
+    val DiscordLogo = IconLoader.getIcon("/discord_logo.png", javaClass) // 16x16 png file
+}
