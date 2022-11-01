4ff90aa3a24b33aa895d334a2610eef590ff6ace
Piop2 <seungyounyou0220@gmail.com>
2022-11-02 03:15:23 +0900

    rename fun

--- src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/Connect.kt
+++ src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/Connect.kt
@@ -2,11 +2,10 @@ package com.Ttakttae.Jetbrains_Discord_Connect.Discord
 
 import com.intellij.openapi.application.PreloadingActivity
 import com.intellij.openapi.progress.ProgressIndicator
-import com.Ttakttae.Jetbrains_Discord_Connect.Discord.SetActivity
 
 class Connect : PreloadingActivity() {
     override fun preload(p0: ProgressIndicator) {
         val setActivity = SetActivity()
-        setActivity.set_activity()
+        setActivity.updateActivity()
     }
 }
--- src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/SetActivity.kt
+++ src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/SetActivity.kt
@@ -4,13 +4,11 @@ import de.jcm.discordgamesdk.Core
 import de.jcm.discordgamesdk.CreateParams
 import de.jcm.discordgamesdk.activity.Activity
 import de.jcm.discordgamesdk.activity.ActivityType
-
 import java.io.File
-import java.io.IOException
 import java.time.Instant
 
 class SetActivity {
-    fun set_activity() {
+    fun updateActivity() {
         Core.init(File("C:\\github.com\\Ttakttae\\Jetbrains-Discord-Connect\\discord_game_sdk_v2\\lib\\x86_64\\discord_game_sdk.dll"))
         CreateParams().use { params ->
             params.clientID = 1036849907954368563L
@@ -20,6 +18,7 @@ class SetActivity {
                     activity.details = "Testing NEW PLUGIN"
                     activity.state = "Code With Me"
                     activity.type = ActivityType.PLAYING
+
                     activity.timestamps().start = Instant.now()
 
                     activity.assets().largeImage = "intellij_idea_logo"
@@ -30,6 +29,7 @@ class SetActivity {
                     activity.party().size().maxSize = 10
                     activity.party().size().currentSize = 1
                     activity.party().id = "Party!"
+
                     activity.secrets().joinSecret = "Join!"
 
                     core.activityManager().updateActivity(activity)
