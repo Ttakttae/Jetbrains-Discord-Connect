00cff42aff283bac9dd4b32f9d01106d07cec164
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-11-01 18:57:49 +0900

    upload gamesdk_v2, update setting activity(failed working with setLargeImage, working later)

--- src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/SetActivity.kt
+++ src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/SetActivity.kt
@@ -4,38 +4,30 @@ import de.jcm.discordgamesdk.Core
 import de.jcm.discordgamesdk.CreateParams
 import de.jcm.discordgamesdk.activity.Activity
 import de.jcm.discordgamesdk.activity.ActivityType
-import com.Ttakttae.Jetbrains_Discord_Connect.Icons.Icons
+
 import java.io.File
 import java.io.IOException
 import java.time.Instant
 
 class SetActivity {
     fun set_activity() {
-        Core.init(File("C:\\github.com\\Ttakttae\\Jetbrains-Discord-Connect\\src\\main\\lib\\x86_64\\discord_game_sdk.dll"))
+        Core.init(File("/Users/hyunwoo/Documents/Coding_Projects/Jetbrains-Discord-Connect/discord_game_sdk_v2/lib/x86_64/discord_game_sdk.dylib"))
         CreateParams().use { params ->
             params.clientID = 1036849907954368563L
             params.flags = CreateParams.getDefaultFlags()
             Core(params).use { core ->
                 Activity().use { activity ->
-                    activity.details = "Running an example"
-                    activity.state = "and having fun"
+                    activity.details = ""
+                    activity.state = ""
                     activity.type = ActivityType.PLAYING
-
-                    // Setting a start time causes an "elapsed" field to appear
                     activity.timestamps().start = Instant.now()
+                    activity.assets().largeImage = "https://cdn.discordapp.com/app-assets/1036849907954368563/intellij_idea_logo.png"
 
-//                    // We are in a party with 10 out of 100 people.
-                    activity.party().size().maxSize = 100
-                    activity.party().size().currentSize = 10
-//
-//                    // Make a "cool" image show up
-                    activity.assets().largeImage = "C:\\github.com\\Ttakttae\\Jetbrains-Discord-Connect\\src\\main\\resources\\discord_logo.png"
-//
-//                    // Setting a join secret and a party ID causes an "Ask to Join" button to appear
+                    activity.party().size().maxSize = 10
+                    activity.party().size().currentSize = 1
                     activity.party().id = "Party!"
                     activity.secrets().joinSecret = "Join!"
 
-                    // Finally, update the current activity to our activity
                     core.activityManager().updateActivity(activity)
                 }
 
