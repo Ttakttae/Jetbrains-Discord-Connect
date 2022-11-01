df71990bc640c8184bbe570fc61c36a72c69e8a2
Piop2 <seungyounyou0220@gmail.com>
2022-11-01 13:01:22 +0900

    set act

--- src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/SetActivity.kt
+++ src/main/kotlin/com/Ttakttae/Jetbrains_Discord_Connect/Discord/SetActivity.kt
@@ -4,16 +4,16 @@ import de.jcm.discordgamesdk.Core
 import de.jcm.discordgamesdk.CreateParams
 import de.jcm.discordgamesdk.activity.Activity
 import de.jcm.discordgamesdk.activity.ActivityType
-
+import com.Ttakttae.Jetbrains_Discord_Connect.Icons.Icons
 import java.io.File
 import java.io.IOException
 import java.time.Instant
 
 class SetActivity {
     fun set_activity() {
-        Core.init(File("/Users/hyunwoo/Documents/Coding_Projects/Jetbrains-Discord-Connect/discord_game_sdk/lib/x86_64/discord_game_sdk.dylib"))
+        Core.init(File("C:\\github.com\\Ttakttae\\Jetbrains-Discord-Connect\\src\\main\\lib\\x86_64\\discord_game_sdk.dll"))
         CreateParams().use { params ->
-            params.clientID = 783147071808471090L
+            params.clientID = 1036849907954368563L
             params.flags = CreateParams.getDefaultFlags()
             Core(params).use { core ->
                 Activity().use { activity ->
@@ -25,15 +25,15 @@ class SetActivity {
                     activity.timestamps().start = Instant.now()
 
 //                    // We are in a party with 10 out of 100 people.
-//                    activity.party().size().maxSize = 100
-//                    activity.party().size().currentSize = 10
+                    activity.party().size().maxSize = 100
+                    activity.party().size().currentSize = 10
 //
 //                    // Make a "cool" image show up
-//                    activity.assets().largeImage = "test"
+                    activity.assets().largeImage = "C:\\github.com\\Ttakttae\\Jetbrains-Discord-Connect\\src\\main\\resources\\discord_logo.png"
 //
 //                    // Setting a join secret and a party ID causes an "Ask to Join" button to appear
-//                    activity.party().id = "Party!"
-//                    activity.secrets().joinSecret = "Join!"
+                    activity.party().id = "Party!"
+                    activity.secrets().joinSecret = "Join!"
 
                     // Finally, update the current activity to our activity
                     core.activityManager().updateActivity(activity)
